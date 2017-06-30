6cbd3a21bfd653b440833667455bfad06159c93a
Romain Sarry <romain.sarry@gmail.com>
2017-06-30 10:03:08 +0200

    User table

--- src/SporFonyBundle/Entity/User.php
+++ src/SporFonyBundle/Entity/User.php
@@ -22,4 +22,62 @@ class User
     {
         return $this->id;
     }
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
